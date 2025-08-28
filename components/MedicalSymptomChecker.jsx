4177fa610ee8c2d5d6d2d9933d74d24f1a7cef63
DTAB85 <adambrown.mba@gmail.com>
2025-08-28 11:48:44 -0500

    Create MedicalSymptomChecker.jsx

---
+++ components/MedicalSymptomChecker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MedicalSymptomChecker() {
+  return (
+    <div style={{ padding: '40px', fontSize: '24px', textAlign: 'center' }}>
+      ✅ Your Symptom Checker is Live!
+    </div>
+  );
+}
